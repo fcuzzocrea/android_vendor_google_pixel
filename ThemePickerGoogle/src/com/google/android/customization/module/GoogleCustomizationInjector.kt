9d9b9dc00271dc4dabd29039d4da24a7838eebe2
Anay Wadhera <awadhera@berkeley.edu>
2022-04-16 19:32:19 -0700

    google_pixel: ThemePickerGoogle: Implement default customization sections
    Change-Id: I8d7390a3710883688028b22e7027f8a4002f40c8

--- ThemePickerGoogle/src/com/google/android/customization/module/GoogleCustomizationInjector.kt
+++ ThemePickerGoogle/src/com/google/android/customization/module/GoogleCustomizationInjector.kt
@@ -1,5 +1,18 @@
 package com.google.android.customization.module
 
+import com.android.wallpaper.module.CustomizationSections
+
 import com.android.customization.module.DefaultCustomizationInjector
 
-public class GoogleCustomizationInjector : DefaultCustomizationInjector()
+public class GoogleCustomizationInjector : DefaultCustomizationInjector() {
+
+    private var customizationSections: CustomizationSections? = null
+
+    override fun getCustomizationSections(): CustomizationSections {
+        if (customizationSections == null) {
+            customizationSections = GoogleCustomizationSections()
+        }
+        return customizationSections!!
+    }
+
+}
